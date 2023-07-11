caf5b0884f2f86b8e4b14fa6abf858682436759a
Edit22 <linertedyta@gmail.com>
2023-07-11 20:18:15 +0100

    custom section

--- cabin_closeBtn.js
+++ cabin_closeBtn.js
@@ -1,23 +1,23 @@
-const openMenu = () => {
-    document.querySelector(selectors, '.backdrop').className = 'backdrop active';
-    document.querySelector(selectors, 'aside').className = 'active';
-}
+// const openMenu = () => {
+//     document.querySelector(selectors, '.backdrop').className = 'backdrop active';
+//     document.querySelector(selectors, 'aside').className = 'active';
+// }
 
-const closeMenu = () => {
-    document.querySelector(selectors,'.backdrop').className = 'backdrop';
-    document.querySelector(selectors,'aside').className = '';
-}
+// const closeMenu = () => {
+//     document.querySelector(selectors,'.backdrop').className = 'backdrop';
+//     document.querySelector(selectors,'aside').className = '';
+// }
 
 
-document.getElementById(elementId, 'menuBtn').onclick = e => {
-    e.preventDefault();
-    openMenu();
-}
+// document.getElementById(elementId, 'menuBtn').onclick = e => {
+//     e.preventDefault();
+//     openMenu();
+// }
 
-document.querySelector(selectors, 'aside .close__btn').onclick = e => {
-    closeMenu();
-}
+// document.querySelector(selectors, 'aside .close__btn').onclick = e => {
+//     closeMenu();
+// }
 
-document.querySelector(selectors, 'backdrop').onclick = e => {
-    closeMenu();
-}
+// document.querySelector(selectors, 'backdrop').onclick = e => {
+//     closeMenu();
+// }
