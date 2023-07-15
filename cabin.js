568cce43e44f35e3c15747f90cfadf1a47e75e94
Edit22 <linertedyta@gmail.com>
2023-07-15 21:25:13 +0100

    Slideshow JS added

--- cabin.js
+++ cabin.js
@@ -1,11 +1,28 @@
-const btn = document.querySelector(".menuBtn");
-const aside = document.querySelector(".menu__aside");
-const btnClose = document.querySelector(".close__btn");
+// const btn = document.querySelector(".menuBtn");
+// const aside = document.querySelector(".menu__aside");
+// const btnClose = document.querySelector(".close__btn");
 
-btn.addEventListener('click', function() {
-    aside.style.display = "block";
-})
+// btn.addEventListener('click', function() {
+//     aside.style.display = "block";
+// })
 
-btnClose.addEventListener('click', function(){
-    aside.style.display = "none";
-})
+// btnClose.addEventListener('click', function(){
+//     aside.style.display = "none";
+// })
+
+
+const locationSlideImg = document.querySelector('.location__slideImg');
+const locationNextBtn = document.querySelector('.location__nextBtn');
+
+const locationSlides = ["location__images/location_mountain_cabin-1.png", "location__images/location_mountain_cabin-2.png", "location__images/location_mountain_cabin-3.png", "location__images/location_mountain_cabin-4.png", "location__images/location_mountain_cabin-5.png" ];
+let currentSlideIndex = 0;
+
+locationNextBtn.addEventListener('click', function() {
+    currentSlideIndex++;
+    if (currentSlideIndex >= locationSlides.length) {
+       currentSlideIndex = 0; 
+    }
+
+    locationSlideImg.src = locationSlides[currentSlideIndex];
+
+});
