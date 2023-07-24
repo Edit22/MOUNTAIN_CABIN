998bda977e91168344d17d3fe15c690e591314bd
Edit22 <linertedyta@gmail.com>
2023-07-24 19:32:48 +0100

    JS slideshow added

--- cabin.js
+++ cabin.js
@@ -1,3 +1,31 @@
+const cabinImg = document.querySelector('.cabinSlideImg');
+const cabinBtn = document.querySelector('.cabinNextBtn');
+
+
+const cabinSlideshow = ['location_cabin/cabin_1.png', 'location_cabin/cabin_2.png', 'location_cabin/cabin_3.png', 'location_cabin/cabin_4.png', 'location_cabin/cabin_6.png'];
+
+let currentCabinSlideshowIndex = 0;
+
+cabinBtn.addEventListener('click', function(){
+   if (currentCabinSlideshowIndex >= cabinSlideshow.length -1) {
+    currentCabinSlideshowIndex = 0;
+   } else {
+    currentCabinSlideshowIndex++;
+   }
+
+cabinImg.src = cabinSlideshow[currentCabinSlideshowIndex];
+
+});
+
+
+
+
+
+
+
+
+
+
 // const btn = document.querySelector(".menuBtn");
 // const aside = document.querySelector(".menu__aside");
 // const btnClose = document.querySelector(".close__btn");
